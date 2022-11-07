4939fc18269b24e47e5aac7b88799fa4d94bd3bd
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-11-07 14:54:11 +0100

    Added title with jumbotron

---
+++ src/components/AppHero.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+    data(){
+        return{
+            message: "Ciao sono un jumbotron",
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="jumbotron">
+        <img src="../assets/pexels-pixabay-163036.jpg" alt="">
+        <div class="jumbo-message">
+            <h1>{{ message }}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ipsa. Lorem ipsum dolor sit amet.</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.jumbotron{
+    width: 100%;
+    height: 500px;
+    position: relative;
+}
+
+.jumbotron img{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.jumbotron .jumbo-message{
+    text-align: right;
+    position: absolute;
+    top: 10px;
+    right: 25px;
+    color: #fff;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+    data(){
+        return{
+            greeting: "Hello World!!!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>{{ greeting }}</h1>
+</template>
+
+<style></style>
